e02e9e7e118772b0f28575a513b1ed4db7b72d4e
Balázs Kiss <dev@balazskiss.name>
2015-08-06 17:14:21 +0100

    graph server with flask

---
+++ twittergraphserver.py
@@ -0,0 +1,43 @@
+import networksize
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+app.debug = True
+network = None
+crawler = None
+
+@app.route("/")
+def index():
+    info = {"nodes":network.GetNodes(), "edges":network.GetEdges()}
+    return jsonify({"result": info})
+
+@app.route("/randomNode")
+def randomNode():
+    node = crawler.getRandomNode()
+    return jsonify({"result": node})
+
+@app.route("/highestDegreeNode")
+def highestDegreeNode():
+    node = crawler.getHighestDegreeNode()
+    return jsonify({"result": node})
+
+@app.route("/connectedNodes/<node>")
+def connectedNodes(node):
+    connectedNodes = crawler.getConnectedNodes(int(node))
+    return jsonify({"result": connectedNodes})
+
+@app.route("/degreeOfNode/<node>")
+def degreeOfNode(node):
+    degree = crawler.getDegreeOfNode(int(node))
+    return jsonify({"result": degree})
+
+
+if __name__ == '__main__':
+    # Twitter network sample
+    # 41.7 million user profiles, 1.47 billion social relations
+    importer = networksize.GraphImporter("/Volumes/Blaze's Disk/twitter/twitter_rv.net", 4561230)
+    network = importer.importGraph(False, 10000)
+    crawler = networksize.SnapGraphCrawler(network)
+
+    app.run()
