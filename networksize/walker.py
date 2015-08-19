72ca48fde0b64e422c1de4381c856d6bbc6475df
Balázs Kiss <dev@balazskiss.name>
2015-08-20 00:32:14 +0100

    syntax fix

--- networksize/walker.py
+++ networksize/walker.py
@@ -2,74 +2,76 @@ import snap
 import random
 from random import randint
 
+
 class RandomWalkerDelegate:
 
     def __init__(self):
         pass
 
-    def returnedToStartNode(self, step):
+    def returned_to_start_node(self, step):
         pass
+
 
 class RandomWalker:
 
-    def __init__(self, crawler, estimator, startNode):
+    def __init__(self, crawler, estimator, start_node):
         self.delegate = None
         self.crawler = crawler
         self.estimator = estimator
-        self.startNode = startNode
+        self.start_node = start_node
+        self.current_node = None
         self.network = snap.TNEANet.New()
         self.running = False
-        self.visitedNodes = set()
+        self.visited_nodes = set()
+        self.step = 0
 
-    def chooseNextNodeRandomlyUsingWeights(self):
-        connectedNodesWithDegrees = self.crawler.getConnectedNodesWithDegrees(self.currentNode)
-        du = len(connectedNodesWithDegrees)
+    def choose_next_node_randomly_using_weights(self):
+        connected_nodes_with_degrees = self.crawler.getConnectedNodesWithDegrees(self.current_node)
+        du = len(connected_nodes_with_degrees)
 
         # Weighted Random Walk
         rnd = random.random()
         psum = 0.0
-        w_node = self.estimator.getNodeWeight(self.currentNode, du, connectedNodesWithDegrees)
-        for neighbour in connectedNodesWithDegrees:
-            dv = connectedNodesWithDegrees[neighbour]
-            w_edge = self.estimator.getEdgeWeight(self.currentNode, neighbour, du, dv)
+        w_node = self.estimator.getNodeWeight(self.current_node, du, connected_nodes_with_degrees)
+        for neighbour in connected_nodes_with_degrees:
+            dv = connected_nodes_with_degrees[neighbour]
+            w_edge = self.estimator.getEdgeWeight(self.current_node, neighbour, du, dv)
             p = w_edge / w_node
             psum += p
             if rnd <= psum:
                 return neighbour
         return None
 
-    def chooseNextNodeRandomly(self):
-        connectedNodes = self.crawler.getConnectedNodes(self.currentNode)
+    def choose_next_node_randomly(self):
+        connected_nodes = self.crawler.getConnectedNodes(self.current_node)
 
         # Simple Random Walk
-        randomIndex = randint(0, len(connectedNodes)-1)
-        nextNode = connectedNodes[randomIndex]
-        return nextNode
+        random_index = randint(0, len(connected_nodes)-1)
+        next_node = connected_nodes[random_index]
+        return next_node
 
-    def chooseNextNode(self):
-        randomWalkType = self.estimator.randomWalkType()
-        if randomWalkType == "simple":
-            return self.chooseNextNodeRandomly()
-        elif randomWalkType == "weighted":
-            return self.chooseNextNodeRandomlyUsingWeights()
+    def choose_next_node(self):
+        random_walk_type = self.estimator.randomWalkType()
+        if random_walk_type == "simple":
+            return self.choose_next_node_randomly()
+        elif random_walk_type == "weighted":
+            return self.choose_next_node_randomly_using_weights()
 
     def walk(self):
         self.running = True
         self.step = 0
-        self.currentNode = self.startNode
-        self.visitedNodes.add(self.currentNode)
+        self.current_node = self.start_node
+        self.visited_nodes.add(self.current_node)
         while self.running:
             # Choose next node
-            self.currentNode = self.chooseNextNode()
-            self.visitedNodes.add(self.currentNode)
+            self.current_node = self.choose_next_node()
+            self.visited_nodes.add(self.current_node)
             self.step += 1
 
-            if self.currentNode == self.startNode :
-                if self.delegate != None:
-                    self.delegate.returnedToStartNode(self.step)
+            if self.current_node == self.start_node:
+                if self.delegate is not None:
+                    self.delegate.returned_to_start_node(self.step)
                 self.step = 0
-
-
 
     def stop(self):
         self.running = False
